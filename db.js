65986e914a2b1c1bbcd4557a5dec9ac2f33cc9bf
ruth1solomon <ruthsolomon2021@gmail.com>
2024-11-25 15:15:41 +0300

    more

--- db.js
+++
@@ -1,4 +0,0 @@
-const mongoose=require'mongoose';
-
-var mongoUrl='mongodb+srv://ruthwoldesemait:iloveJesus@cluster0.43voo.mongodb.net/';
-mongoose.connect(mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true});
